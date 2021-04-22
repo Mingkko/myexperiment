747d20b223b9a158ec62e33fe5db0c983558bba2
mingkko <mingkkozz@gmail.com>
2021-04-22 18:42:08 +0800

    update

--- ner/bilstm_crf.py
+++ ner/bilstm_crf.py
@@ -10,7 +10,14 @@ from bert4keras.layers import ConditionalRandomField
 from keras.layers import Dense
 from keras.models import Model
 from tqdm import tqdm
-import jieba
+
+epochs = 10
+MAXLEN = 200
+crf_lr_multiplier = 1000
+batch_size = 32
+learning_rate = 3e-4
+
+dict_path = '../pretrain_model/chinese_roberta/vocab.txt'
 
 def load_data(filename):
     D = []
@@ -37,25 +44,156 @@ def load_data(filename):
     return D
 
 train_data = load_data('./data/example.train')
-dev_data = load_data('./data/example.dev')
+valid_data = load_data('./data/example.dev')
 test_data = load_data('./data/example.test')
 
-tokenizer = Tokenizer(token_dict=)
+tokenizer = Tokenizer(token_dict=dict_path,do_lower_case=True)
 
-text = '我爱中国'
-temp = tokenizer.tokenize(text)
-print(temp)
+# text = '我爱中国'
+# temp = tokenizer.tokenize(text)
+# print(temp)
 
-exit(0)
+# exit(0)
 #类别映射
 labels = ['PER','LOC','ORG']
 id2label = dict(enumerate(labels))
 label2id = {j:i for i,j in id2label.items()}
 num_labels = len(labels)*2 +1 #tag*BI + O
 
-class data_generater(DataGenerator):
+class data_generator(DataGenerator):
 
-    def __init__(self,random=False):
+    def __iter__(self,random=False):
         batch_token_ids, batch_labels = [], []
         for is_end, item in self.sample(random):
-            break
+            token_ids, labels = [tokenizer._token_start_id], [0]
+            for w, l in item:
+                w_token_ids = tokenizer.encode(w)[0][1:-1]
+                if len(token_ids) + len(w_token_ids) < MAXLEN:
+                    token_ids += w_token_ids
+                    if l == 'O':
+                        labels += [0] * len(w_token_ids)
+                    else:
+                        B = label2id[l]*2 + 1
+                        I = label2id[l]*2 + 2
+                        labels += ([B] + [I]*(len(w_token_ids)-1))
+                else:
+                    break
+            token_ids += [tokenizer._token_end_id]
+            labels += [0]
+            batch_token_ids.append(token_ids)
+            batch_labels.append(labels)
+            if len(batch_token_ids) == self.batch_size or is_end:
+                batch_token_ids = sequence_padding(batch_token_ids,length=MAXLEN)
+                batch_labels = sequence_padding(batch_labels,length=MAXLEN)
+                yield batch_token_ids, batch_labels
+                batch_token_ids, batch_labels = [], []
+
+model = keras.models.Sequential()
+model.add(keras.layers.Embedding(21129,1024,input_length=MAXLEN))
+model.add(keras.layers.Bidirectional(keras.layers.LSTM(1024,return_sequences=True)))
+model.add(keras.layers.Dropout(0.1))
+model.add(keras.layers.Bidirectional(keras.layers.LSTM(1024,return_sequences=True)))
+model.add(Dense(num_labels))
+CRF = ConditionalRandomField(lr_multiplier=crf_lr_multiplier)
+output = model.output
+output = CRF(output)
+
+model = Model(model.input, output)
+model.summary()
+
+model.compile(
+    loss=CRF.sparse_loss,
+    optimizer=Adam(learning_rate),
+    metrics=[CRF.sparse_accuracy]
+)
+
+
+class nameentityrecognizer(ViterbiDecoder):
+
+    def recognize(self, text):
+        tokens = tokenizer.tokenize(text)
+        while len(tokens)>512:
+            tokens.pop(-2)
+        mapping = tokenizer.rematch(text, tokens)
+        tokens_ids = tokenizer.tokens_to_ids(tokens)
+        tokens_ids = to_array([tokens_ids])
+        nodes = model.predict(tokens_ids)[0]
+        labels = self.decode(nodes)
+        entities, starting = [], False
+        for i, label in enumerate(labels):
+            if label > 0 :
+                if label %2 ==1:
+                    starting = True
+                    entities.append([[i], id2label[(label-1)//2]])
+                elif starting:
+                    entities[-1][0].append(i)
+                else:
+                    starting = False
+            else:
+                starting = False
+
+        return [(text[mapping[w[0]][0]:mapping[w[-1]][-1] + 1], 1)
+                for w, l in entities]
+
+NER = nameentityrecognizer(trans=K.eval(CRF.trans), starts=[0], ends=[0])
+
+def evaluate(data):
+
+    X, Y, Z = 1e-10, 1e-10, 1e-10
+    for d in tqdm(data):
+        text = ''.join([i[0] for i in d])
+        R = set(NER.recognize(text))
+        T = set([tuple(i) for i in d if i[1] !='O'])
+        X +=len(R & T)
+        Y +=len(R)
+        Z +=len(T)
+    f1, precision, recall = 2*X/(Y+Z), X/Y, X/Z
+    return f1, precision, recall
+
+class Evaluator(keras.callbacks.Callback):
+    """评估与保存
+    """
+    def __init__(self):
+        self.best_val_f1 = 0
+
+    def on_epoch_end(self, epoch, logs=None):
+        trans = K.eval(CRF.trans)
+        NER.trans = trans
+        print(NER.trans)
+        f1, precision, recall = evaluate(valid_data)
+        # 保存最优
+        if f1 >= self.best_val_f1:
+            self.best_val_f1 = f1
+            model.save_weights('./best_model.weights')
+        print(
+            'valid:  f1: %.5f, precision: %.5f, recall: %.5f, best f1: %.5f\n' %
+            (f1, precision, recall, self.best_val_f1)
+        )
+        f1, precision, recall = evaluate(test_data)
+        print(
+            'test:  f1: %.5f, precision: %.5f, recall: %.5f\n' %
+            (f1, precision, recall)
+        )
+
+if __name__ == '__main__':
+
+    evaluator = Evaluator()
+    train_generator = data_generator(train_data, batch_size)
+
+    # for b in train_generator:
+    #     t_ids, labels = b[0],b[1]
+    #     print(t_ids.shape)
+    #     print(t_ids)
+    #     break
+
+    model.fit(
+        train_generator.forfit(),
+        steps_per_epoch=len(train_generator),
+        epochs=epochs,
+        callbacks=[evaluator]
+    )
+
+else:
+
+    model.load_weights('./best_model.weights')
+    NER.trans = K.eval(CRF.trans)
